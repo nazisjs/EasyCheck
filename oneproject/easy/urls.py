200ee2fa0987566f420b059e49ef1e4c38befb2e
nazerke <mnazerke89@gmail.com>
2025-06-08 00:48:07 +0500

    upd2

--- oneproject/easy/urls.py
+++ oneproject/easy/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path("",views.loginUser,name="home"),
+    path("dashboard/",views.dashboard,name="dashboard")
 ]
--- oneproject/easy/views.py
+++ oneproject/easy/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def loginUser(request):
     return render(request,"login.html")
+
+def dashboard(request):
+    return render(request,"dashboard.html")
